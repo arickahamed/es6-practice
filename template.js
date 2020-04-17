1379d7eefd1ea60f38697e3a27a8e6af9ff3a3fe
arickahamed <arickahamed700@gmail.com>
2020-04-17 16:47:33 +0600

    template

---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "jodu";
+const lastName = "mia";
+// const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${20+30+50} is a good boy.`
+console.log(fullName2);
+
+
+const multiline = `i am jodi mia . i work in a farm and earn money for my family.`
+console.log(multiline);
+
+const multiline2 = `i am modhu mia . i am jodu mia's brother.
+i live in england.
+i came here about 30 years ago .
+i love bangladesh.`
+console.log(multiline2);
